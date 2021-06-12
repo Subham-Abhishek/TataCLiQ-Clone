797d16bc756522867ec94ea9d10b8eaae6670cff
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-06-12 19:21:35 +0530

    Login/SignUp page added

--- Script/main.js
+++ Script/main.js
@@ -122,3 +122,28 @@ window.addEventListener('scroll',function(){
     section[+a].classList.remove('show')
 }
 
+// login signup page
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+// signIn signup page render
+function ope(){
+    var cover = document.querySelector('.cover')
+    cover.style.display='block'
+}
+function clos(){
+    var cover = document.querySelector('.cover')
+    cover.style.display='none'
+}
+
+
